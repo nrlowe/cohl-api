17257990497e4ed4f4b5da80c8cf29ce47da5fb1
nrlowe <nate.lowe.2013@owu.edu>
2024-05-28 16:21:55 -0400

    first

---
+++ src/test/java/com/cohleague/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cohleague.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
