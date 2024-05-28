bf5a06b351c371cf0de2172239e9ebbca66ccd7d
nrlowe <nate.lowe.2013@owu.edu>
2024-05-28 16:29:33 -0400

    Moving files over from old project

---
+++ src/main/java/com/cohleague/api/DataSourceConfig.java
@@ -0,0 +1,39 @@
+package com.cohleague.api;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @Value("${spring.datasource.driver-class-name}")
+    private String driverClassName;
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(driverClassName);
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        // byte[] decodedBytes = Base64.getDecoder().decode(password);
+        // new String(decodedBytes, StandardCharsets.UTF_8)
+        dataSource.setPassword(password);
+        return dataSource;
+    }
+}
