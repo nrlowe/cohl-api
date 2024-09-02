a16592179f632b6d982f8f2dd457ec151eb1d47c
nrlowe <nate.lowe.2013@owu.edu>
2024-09-01 21:10:16 -0400

    Adding swagger

---
+++ src/main/java/com/cohleague/api/OpenApiConfig.java
@@ -0,0 +1,26 @@
+package com.cohleague.api;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("My Spring Boot API")
+                        .description("This is the API documentation for My Spring Boot Application")
+                        .version("v1.0.0")
+                        .contact(new Contact().name("John Doe").email("johndoe@example.com"))
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("More Documentation")
+                        .url("https://example.com/docs"));
+    }
+}
